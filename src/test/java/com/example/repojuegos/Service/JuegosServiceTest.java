e5427b6c331a5b5c92380390052a8eba49c15f02
gabe <gabrielcorycarl@proton.me>
2026-05-15 19:20:31 +0100

    fix: corregir valor

--- src/test/java/com/example/repojuegos/Service/JuegosServiceTest.java
+++ src/test/java/com/example/repojuegos/Service/JuegosServiceTest.java
@@ -27,7 +27,7 @@ public class JuegosServiceTest {
     void setUp() {
         juego = new Juegos();
         juego.setId(5L);
-        juego.setNombre("Minecraft");
+        juego.setNombre("God of War");
     }
 
     @Test
